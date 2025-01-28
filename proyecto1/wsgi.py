0a987b3ad509e39021864c6b59e4813dbf07d3fd
josemabp1 <46225139+josemabp1@users.noreply.github.com>
2025-01-28 20:23:00 +0100

    Update wsgi.py

--- proyecto1/wsgi.py
+++ proyecto1/wsgi.py
@@ -1,10 +1,16 @@
-import os
-import sys
+"""
+WSGI config for proyecto1 project.
 
-path = os.path.expanduser('~/jmbejarano/proyecto1')
-if path not in sys.path:
-    sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'proyecto1.settings'
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto1.settings')
+
+application = get_wsgi_application()
