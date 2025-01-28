571b147c7b52feb4faca34659d7df38a14112431
josemabp1 <46225139+josemabp1@users.noreply.github.com>
2025-01-28 08:52:22 +0100

    Update settings.py

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#2^-en&-)vn3b6lg%rrly&sx13+&&ag5o($f46p344#6s3(i4w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'jmbejarano.pythonanywhere.com']
 
 
 # Application definition
